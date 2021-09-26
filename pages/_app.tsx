bf881005933cfb28e21fc7f12b8026d4922d1485
avosalmon <avosalmon@gmail.com>
2021-09-26 12:13:42 +0800

    add axios interceptors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import "../libs/axiosInterceptors";
 import "../styles/globals.css";
 
 type NextPageWithLayout = NextPage & {
