da40383a84fd575f6384a91ec4acabb54108e5f5
avosalmon <avosalmon@gmail.com>
2021-10-02 18:04:22 +0800

    render post list

--- layouts/dashboard.tsx
+++ layouts/dashboard.tsx
@@ -5,7 +5,6 @@ import { useAuthContext } from "../hooks/useAuthContext";
 
 interface Props {
   children: ReactNode;
-  title: string;
 }
 
 const navigation = [
@@ -23,7 +22,7 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Dashboard({ children, title }: Props): JSX.Element {
+export default function Dashboard({ children }: Props): JSX.Element {
   const { user } = useAuthContext();
 
   if (!user) {
@@ -191,13 +190,6 @@ export default function Dashboard({ children, title }: Props): JSX.Element {
         )}
       </Disclosure>
 
-      <header className="bg-white shadow-sm">
-        <div className="px-4 py-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <h1 className="text-lg font-semibold leading-6 text-gray-900">
-            {title}
-          </h1>
-        </div>
-      </header>
       <main>
         <div className="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="px-4 py-4 sm:px-0">{children}</div>
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,15 +1,90 @@
 import React, { ReactElement, ReactNode } from "react";
+import Link from "next/link";
 import { NextPageWithLayout } from "../_app";
 import { Dashboard, Protected } from "../../layouts";
+import usePostCollection from "../../hooks/usePostCollection";
+import { formatDate } from "../../libs/helper";
 
 const Posts: NextPageWithLayout = () => {
-  return <div>Posts</div>;
+  const { posts, loading } = usePostCollection();
+
+  if (loading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (!posts) {
+    return <div>Failed to load posts.</div>;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div className="pt-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-t-lg">
+          <table className="min-w-full">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Title
+                </th>
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Published At
+                </th>
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Updated At
+                </th>
+                <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Status
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.data.map((post, index) => (
+                <tr
+                  key={post.id}
+                  className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                >
+                  <td className="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-nowrap">
+                    {post.title}
+                  </td>
+                  <td className="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-nowrap">
+                    {post.published_at && formatDate(post.published_at)}
+                  </td>
+                  <td className="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-nowrap">
+                    {post.updated_at && formatDate(post.updated_at)}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    {post.is_published ? (
+                      <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                        Published
+                      </span>
+                    ) : (
+                      <span className="inline-flex px-2 text-xs font-semibold leading-5 text-red-800 bg-red-100 rounded-full">
+                        Draft
+                      </span>
+                    )}
+                  </td>
+                  <td className="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-nowrap">
+                    <Link href={`/posts/${post.id}`}>
+                      <a className="text-indigo-600 hover:text-indigo-900">
+                        Edit
+                      </a>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Posts.getLayout = (page: ReactElement): ReactNode => {
   return (
     <Protected>
-      <Dashboard title="Your posts">{page}</Dashboard>
+      <Dashboard>{page}</Dashboard>
     </Protected>
   );
 };
