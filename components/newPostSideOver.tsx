22f1a6bd34556e1126efdd9527b78471fd0f4320
avosalmon <avosalmon@gmail.com>
2021-10-18 21:08:30 +0800

    extract test input component

--- components/newPostSideOver.tsx
+++ components/newPostSideOver.tsx
@@ -1,6 +1,9 @@
 import { forwardRef, Fragment, useImperativeHandle, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import TextInput from "./textInput";
 
 export interface NewPostSideOverRef {
   open: () => void;
@@ -23,6 +26,11 @@ const NewPostSideOver = forwardRef<NewPostSideOverRef>((props, ref) => {
     []
   );
 
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Post title is required"),
+    slug: Yup.string().required("Post slug is required"),
+  });
+
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -44,87 +52,98 @@ const NewPostSideOver = forwardRef<NewPostSideOverRef>((props, ref) => {
               leaveTo="translate-x-full"
             >
               <div className="w-screen max-w-md">
-                <form className="flex flex-col h-full bg-white divide-y divide-gray-200 shadow-xl">
-                  <div className="flex-1 h-0 overflow-y-auto">
-                    <div className="px-4 py-6 bg-indigo-700 sm:px-6">
-                      <div className="flex items-center justify-between">
-                        <Dialog.Title className="text-lg font-medium text-white">
-                          New Post
-                        </Dialog.Title>
-                        <div className="flex items-center ml-3 h-7">
-                          <button
-                            type="button"
-                            className="text-indigo-200 bg-indigo-700 rounded-md hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                            onClick={() => setOpen(false)}
-                          >
-                            <span className="sr-only">Close panel</span>
-                            <XIcon className="w-6 h-6" aria-hidden="true" />
-                          </button>
-                        </div>
-                      </div>
-                      <div className="mt-1">
-                        <p className="text-sm text-indigo-300">
-                          Get started by filling in the information below to
-                          write your new post. Don't worry, you can change them
-                          any time later.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="flex flex-col justify-between flex-1">
-                      <div className="px-4 divide-y divide-gray-200 sm:px-6">
-                        <div className="pt-6 pb-5 space-y-6">
-                          <div>
-                            <label
-                              htmlFor="project-name"
-                              className="block text-sm font-medium text-gray-900"
+                <Formik
+                  initialValues={{
+                    title: "",
+                    slug: "",
+                  }}
+                  onSubmit={(values) => {
+                    console.log(values);
+                  }}
+                  validationSchema={validationSchema}
+                >
+                  <Form className="flex flex-col h-full bg-white divide-y divide-gray-200 shadow-xl">
+                    <div className="flex-1 h-0 overflow-y-auto">
+                      <div className="px-4 py-6 bg-indigo-700 sm:px-6">
+                        <div className="flex items-center justify-between">
+                          <Dialog.Title className="text-lg font-medium text-white">
+                            New Post
+                          </Dialog.Title>
+                          <div className="flex items-center ml-3 h-7">
+                            <button
+                              type="button"
+                              className="text-indigo-200 bg-indigo-700 rounded-md hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                              onClick={() => setOpen(false)}
                             >
-                              Post Title
-                            </label>
-                            <div className="mt-1">
-                              <input
-                                type="text"
-                                name="project-name"
-                                id="project-name"
-                                className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500"
-                              />
-                            </div>
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="description"
-                              className="block text-sm font-medium text-gray-900"
-                            >
-                              Post Slug
-                            </label>
-                            <div className="mt-1">
-                              <input
-                                type="text"
-                                name="project-name"
-                                id="project-name"
-                                className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500"
-                              />
-                            </div>
+                              <span className="sr-only">Close panel</span>
+                              <XIcon className="w-6 h-6" aria-hidden="true" />
+                            </button>
                           </div>
                         </div>
+                        <div className="mt-1">
+                          <p className="text-sm text-indigo-300">
+                            Get started by filling in the information below to
+                            write your new post. Don't worry, you can change
+                            them any time later.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex flex-col justify-between flex-1">
+                        <div className="px-4 divide-y divide-gray-200 sm:px-6">
+                          <div className="pt-6 pb-5 space-y-6">
+                            <div>
+                              <label
+                                htmlFor="title"
+                                className="block text-sm font-medium text-gray-900"
+                              >
+                                Post Title
+                              </label>
+                              <div className="mt-1">
+                                <Field
+                                  id="title"
+                                  name="title"
+                                  type="text"
+                                  component={TextInput}
+                                />
+                              </div>
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="slug"
+                                className="block text-sm font-medium text-gray-900"
+                              >
+                                Post Slug
+                              </label>
+                              <div className="mt-1">
+                                <Field
+                                  id="slug"
+                                  name="slug"
+                                  type="text"
+                                  component={TextInput}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="flex justify-end flex-shrink-0 px-4 py-4">
-                    <button
-                      type="button"
-                      className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      onClick={() => setOpen(false)}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center px-4 py-2 ml-4 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Start writing
-                    </button>
-                  </div>
-                </form>
+                    <div className="flex justify-end flex-shrink-0 px-4 py-4">
+                      <button
+                        type="button"
+                        className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => setOpen(false)}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center px-4 py-2 ml-4 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        Start writing
+                      </button>
+                    </div>
+                  </Form>
+                </Formik>
               </div>
             </Transition.Child>
           </div>
